9a499ca1a636a62d6df50efe09e6da839f4d7a1b
Sergio <sergiodmv239123@hotmail.com>
2024-03-06 14:43:32 -0600

    Integrate students form with other views, and setup alembic

---
+++ controllers/classrooms.py
@@ -0,0 +1,2 @@
+def index():
+    return dict(message="hello from students.py")
--- controllers/students.py
+++ controllers/students.py
@@ -1,3 +1,16 @@
-def register_student(request):
+# -*- coding: utf-8 -*-
+
+# request.get_vars and request.post_vars and request.vars
+import json
+
+def index():
+    return dict()
+
+def attendance():
     if not request.env.request_method == 'GET': raise HTTP(403)
-    return response.json({'status': 'success', 'msg': 'hello', 'data': request})
+    return response.json({'status':'success', 'email':auth.user.email})
+
+def save_student():
+    if not request.env.request_method == 'POST': raise HTTP(403)
+    post_vars = json.loads(request.body.read())
+    return response.json({'status': 'success', 'msg': 'hello', 'body': post_vars['fullname']})
